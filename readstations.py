9539ec6127a0e96fc94b40fcf3b592237beefa26
carikka <carissadean@hotmail.com>
2014-12-07 15:27:30 +0000

    Update readstations.py

--- readstations.py
+++ readstations.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as etree
 
 def read_stations():
 	"""
-	Function to read the xml file and print the latitude and longitude
+	Function to read the xml file Stations and return the Stations, latitude and longitude
 	"""
 	xmlD = etree.parse("Stations.xml")
 	root = xmlD.getroot()
@@ -10,34 +10,32 @@ def read_stations():
 	Stations = []
 	lat_value = []
 	long_value = []
-	lat = []
-	long = []
+	latitude = []
+	longitude = []
 	line = []
-	joke = []
-	line_1 = []
 	for item in root.findall('Station'):
 		station = item.get('id')
 		Stations.append(station)
 	for item in root.findall('Station'):
-		latitude = item.find('Latitude').text
-		lat_value.append(latitude) # Here each Latitude value is appended into a list
+		lat1 = item.find('Latitude').text
+		lat_value.append(lat1) # Here each Latitude value is appended into a list
 	for item in lat_value:
 		del lat_value[0] # Deletes the heading "Latitude" from the list
 	for item in lat_value:
 		c = float(item)
 		lat.append(c) # Converts all the values into floats and appends into a list
 	for item in root.findall('Station'):
-		longitude = item.find('Longitude').text
-		long_value.append(longitude)
+		long1 = item.find('Longitude').text
+		long_value.append(long1)
 	for item in long_value:
 		del long_value[0]
 	for item in long_value:
 		d = float(item)
-		long.append(d)
+		longitude.append(d)
 	for item in root.findall('Station'):
 		lines = item.find('Line').text
 		line.append(lines)
-	return Stations, long, lat, line
+	return Stations, longitude, latitude, line
 
 #-----------------------------------------------------------------------------------------
 read_stations()
